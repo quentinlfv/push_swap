b01f472e3bea5543abd66a196f0bf2e47095d5b3
Quentin Lefevre <quelefev@paul-f4Ar7s5.clusters.42paris.fr>
2025-02-06 15:39:19 +0100

    checker

---
+++ checker.c
@@ -0,0 +1,119 @@
+#include "push_swap.h"
+
+char	**get_order(void)
+{
+	char	**order;
+	char	*line;
+	int		size;
+
+	order = NULL;
+	size = 0;
+	line = get_next_line(0);
+	map = add_new_order(order, line, &size);
+	while (line != NULL)
+	{
+		free(line);
+		line = get_next_line(0);
+		if (line != NULL)
+			map = add_new_order(order, line, &size);
+	}
+	return (order);
+}
+
+char	**add_new_order(char **order, char *line, int *size)
+{
+	char	**new_order;
+	int		i;
+
+	i = 0;
+	new_order = malloc(sizeof(char *) * (*size + 2));
+	if (!new_order)
+		return (NULL);
+	while (i < (*size))
+	{
+		new_order[i] = order[i];
+		i++;
+	}
+	new_order[i] = malloc(sizeof(char) * (ft_strlen(line)));
+	if (!new_order[i])
+		return (NULL);
+	ft_strlcpy(new_order[i], line, ft_strlen(line));
+	new_order[i + 1] = NULL;
+	free(order);
+	(*size)++;
+	return (new_order);
+}
+	
+void	exec_order(char **order, t_list **stack_a, t_list **stack_b)
+{
+	int	i;
+
+	i = 0;
+	while (order[i])
+	{
+		if (ft_strcmp(order[i], "pa"))
+			ft_pa(stack_a, stack_b);
+		if (ft_strcmp(order[i], "pb"))
+			ft_pb(stack_a, stack_b);
+		if (ft_strcmp(order[i], "sa"))
+			ft_sa(stack_a, stack_b);
+		if (ft_strcmp(order[i], "sb"))
+			ft_sb(stack_a, stack_b);
+		if (ft_strcmp(order[i], "ra"))
+			ft_ra(stack_a, stack_b);
+		if (ft_strcmp(order[i], "rb"))
+			ft_rb(stack_a, stack_b);
+		if (ft_strcmp(order[i], "rra"))
+			ft_rra(stack_a, stack_b);
+		if (ft_strcmp(order[i], "rrb"))
+			ft_rrb(stack_a, stack_b);
+		i++;
+	}
+}
+
+int	check_tri(t_list *stack)
+{
+	t_list	*tmp;
+
+	tmp = stack;
+	while (tmp->next)
+	{
+		if (tmp->content > tmp->next->content)
+			return (0);
+		tmp = tmp->next;
+	}
+	return (1);
+}
+
+void	free_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i] != NULL)
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+}
+
+int	main(int ac, char **av)
+{
+	char	**order;
+	t_list *stack_a;
+	t_list *stack_b;
+
+	stack_a = ft_stacknew(ft_atoi(av[i]));
+	stack_b = NULL;
+	while (++1 < ac)
+		ft_stackadd_back(&stack_a, ft_stack_new(ft_atoi(av[i])));
+	order = get_order();
+	exec_order(order, &stack_a, &stack_b);
+	if (!check_tri(stack_a))
+		ft_putstr("KO\n");
+	else
+		ft_putstr("OK\n");
+	free_tab(order);
+	return (0);
+}
